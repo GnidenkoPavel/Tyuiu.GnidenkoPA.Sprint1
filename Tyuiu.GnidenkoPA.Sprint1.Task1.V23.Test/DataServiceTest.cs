d2f0612c654bb87e4d13a914bccb722dc72a3a4a
gnide <gnide@DESKTOP-AE6RANS>
2025-10-01 03:29:25 +0500

    Tusk1

---
+++ Tyuiu.GnidenkoPA.Sprint1.Task1.V23.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.GnidenkoPA.Sprint1.Task1.V23.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint1.Task1.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidTest()
+        {
+            var ds = new DataService();
+            double result = ds.Calculate(1, 2);
+            // (2 * π) / (2 * 1) = π ≈ 3.14159
+            Assert.AreEqual(Math.PI, result, 0.001);
+        }
+    }
+}
---
+++ Tyuiu.GnidenkoPA.Sprint1.Task1.V23/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Tyuiu.GnidenkoPA.Sprint1.Task1.V23.Lib;
+namespace Tyuiu.GnidenkoPA.Sprint1.Task1.V23
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            var ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Гниденко П. А. | ПИНб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #23                                                               *");
+            Console.WriteLine("* Выполнил: Гниденко П. А. | ПИНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, вычисляет результат по формуле (x*Pi)/(2*a) и печатает его на экране.             *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, вычисляет результат по формуле (x*Pi)/(2*a) и печатает его на экране.                                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите x:");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите a:");
+            double a = double.Parse(Console.ReadLine());
+
+            double result = ds.Calculate(a, x);
+
+            Console.WriteLine($"Результат: {result}");
+        }
+    }
+}
